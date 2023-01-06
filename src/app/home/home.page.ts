4331ea8db97e758cedcdaa17dacbc4c009a0f578
samuel <samuelnatan98@gmail.com>
2023-01-06 13:32:32 -0300

    bleApp estruturado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { BleClient, BluetoothLe } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,74 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  devices:any[]=[];
+  ble:boolean=false;
+  scanText:string="";
+  constructor(private change:ChangeDetectorRef) {}  
 
-  constructor() {}
+  ngOnInit(){
+    BleClient.initialize().then(()=>{
+      BleClient.isEnabled().then((res)=>{
+        if(res){
+          this.ble = true;
+        }else {
+          this.ble = false;
+        }
+      })
+    })
+  }
+
+  toogleBle(event) {
+    if(this.ble){
+      this.enableBluetooth();
+    }else {
+      this.disableBluetooth();
+    }
+  }
+  enableBluetooth() {
+    BleClient.enable();
+  }
+  disableBluetooth() {
+    BleClient.disable();
+  }
+  startScanning() {
+    this.scanText = "scanning...";
+
+    BleClient.requestLEScan({allowDuplicates:false}, (res1)=> {
+      if(res1.localName) {
+        this.devices.push(res1);
+        this.change.detectChanges();
+      }
+    })
+
+    setTimeout(()=> {
+      this.stopScanning();
+    },20000)
+  }
+
+  stopScanning() {
+    BluetoothLe.stopLEScan().then(()=>{
+      this.scanText = "";
+    })
+  }
+
+  connect( device, index ) {
+    BleClient.connect(device.device.deviceId).then(()=>{
+      this.devices[index]["connection"] = true;
+      this.change.detectChanges();
+
+      alert("connected!");
+    },(err)=>{
+      alert(err);
+    })
+  }
+
+  disconnect(device, index) {
+    BleClient.disconnect(device.device.deviceId).then(()=>{
+      this.devices[index]["connection"] = false;
+      this.change.detectChanges();
+      alert("disconnected!");
+    })
+  }
 
 }
